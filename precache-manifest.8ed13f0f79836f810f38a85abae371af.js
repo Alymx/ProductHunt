dab3ef082d48158e57366fd49c961e8a6e88ae4a
ALIM <alymict@gmail.com>
2019-10-22 18:04:54 +0100

    premier commit

---
+++ precache-manifest.8ed13f0f79836f810f38a85abae371af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517163b4723c5719eeaff60d95610afd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b73c010ca1f45b24d4c",
+    "url": "/static/css/main.69a3081c.chunk.css"
+  },
+  {
+    "revision": "f97878aab2e851469a4b",
+    "url": "/static/js/2.fd247a62.chunk.js"
+  },
+  {
+    "revision": "1b73c010ca1f45b24d4c",
+    "url": "/static/js/main.b1230d8b.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
